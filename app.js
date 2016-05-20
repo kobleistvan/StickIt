e2a09a6d4ec8278dd539aa91d66d2bb1ac9eb561
Koble Istvan <kobleistvan@gmail.com>
2016-05-20 16:25:39 +0300

    Changed favicon, added winston with 2 transports, added transports conf in config file

--- app.js
+++ app.js
@@ -9,25 +9,50 @@ global.rootRequire = function(name) {
 
 var express = require('express'),
     path = require('path'),
+    config = require('./config.json')[process.env.NODE_ENV || 'dev'],
     favicon = require('serve-favicon'),
-    logger = require('morgan'),
+    morgan = require('morgan'),
+    winston = require('winston'),
+    logger = new winston.Logger({
+        transports: [
+            new winston.transports.File(config.winston.transports.file),
+            new winston.transports.Console(config.winston.transports.console)
+        ],
+        exitOnError: false
+    }),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     helmet = require('helmet'),
-    routes = require('./routes/routes');
+    routes = require('./routes/routes'),
+    app = express();
 
-var app = express();
+// Define a stream to be used by morgan
+logger.stream = {
+    write: function(message, encoding) {
+        logger.info(message);
+    }
+};
+
+// Redirect morgan logs to winston's info stream
+app.use(require("morgan")((process.env.NODE_ENV ? 'combined' : 'dev'), {
+    "stream": logger.stream
+}));
+
+// Say Hi, kids!
+logger.info("Environment set to: ", process.env.NODE_ENV || 'development (dev)');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(favicon(path.join(__dirname, 'public/favicon', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(helmet());
 
+// Handle every route
 app.use('/', routes);
 
 // catch 404 and forward to error handler
