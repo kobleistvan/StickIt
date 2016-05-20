31dd150bb6224692fa2006141e6421da8db9130b
Koble Istvan <kobleistvan@gmail.com>
2016-05-20 15:36:33 +0300

    Restructured route importing and handling

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ var express = require('express'),
     favicon = require('serve-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
+    bodyParser = require('body-parser'),
+    helmet = require('helmet'),
+    routes = require('./routes/routes');
 
 var app = express();
 
@@ -26,14 +26,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -41,23 +42,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 module.exports = app;
---
+++ routes/api/apiRoutes.js
@@ -0,0 +1,8 @@
+var express = require('express'),
+    router = express.Router();
+    // postit = require('./postit');
+
+// Define routes to various API related handlers
+// router.use('/postit', postit);
+
+module.exports = router;
