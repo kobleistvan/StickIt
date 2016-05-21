02d395f6fcfa3abed7b39219f1b653999824b2cc
Koble Istvan <kobleistvan@gmail.com>
2016-05-21 07:40:33 -0700

    Wrote stickynote handlers, without user specification yet

--- routes/api/stickyNote.js
+++ routes/api/stickyNote.js
@@ -1,71 +1,130 @@
 var express = require('express'),
     router = express.Router(),
-    stickyNotes = rootRequire('lib/api/stickyNote'),
-    logger = require('winston');
+    stickyNotes = rootRequire('lib/api/stickyNote');
 
 // RESTful handlers to manage stickynotes
 
-// Returns all the sticky notes
+// Returns every sticky notes
 router.get('/', function(req, res, next) {
-    logger.warn("test");
 
-    res.json({
-        todo: true
-    });
+    // Return every stickynote
+    stickyNotes.getEveryStickyNote(null, function(err, response) {
+        if (err) {
+            return res.json({
+                success: false,
+                message: err
+            })
+        } else {
+            return res.json({
+                success: response.success,
+                stickyNotes: response.stickyNotes
+            })
+        }
+    })
+
 });
 
 // Returns a specific sticky note
 router.get('/:id', function(req, res, next) {
 
-    res.json({
-        todo: true
-    });
+    // Return stickynote
+    stickyNotes.getStickyNote({
+        stickyNoteId: req.params('id')
+    }, function(err, response) {
+        if (err) {
+            return res.json({
+                success: false,
+                message: err
+            })
+        } else {
+            return res.json({
+                success: response.success,
+                stickyNote: response.stickyNote
+            })
+        }
+    })
+
 });
 
-// Creates a new sticky note
+// Creates a new stickynote
 router.post('/', function(req, res, next) {
 
+    // Validate
     if (!req.body.note) {
         return res.json({
             success: false,
-            message: "Sticky note is empty."
-        })
-    } else {
-        stickyNotes.addStickyNote({
-            note: req.body.note
-        }, function(err, response) {
-            if (err) {
-                logger.error("An error occured while creating a new sticky note. Err: ", err);
-
-                return res.json({
-                    success: false,
-                    message: err
-                })
-
-            } else {
-                return res.json({
-                    success: true
-                })
-            }
+            message: "Stickynote is empty."
         })
     }
+
+    // Add new stickynote
+    stickyNotes.addStickyNote({
+        note: req.body.note
+    }, function(err, response) {
+        if (err) {
+            return res.json({
+                success: false,
+                message: err
+            })
+        } else {
+            return res.json({
+                success: response.success,
+                stickyNoteId: response.stickyNoteId
+            })
+        }
+    })
 
 });
 
 // Updates a sticky note
 router.put('/:id', function(req, res, next) {
 
-    res.json({
-        todo: true
-    });
+    // Validate
+    if (!req.body.note) {
+        return res.json({
+            success: false,
+            message: "Stickynote is empty."
+        })
+    }
+
+    // Edit stickynote
+    stickyNotes.editStickyNote({
+        stickyNoteId: req.params('id'),
+        note: req.body.note
+    }, function(err, response) {
+        if (err) {
+            return res.json({
+                success: false,
+                message: err
+            })
+        } else {
+            return res.json({
+                success: response.success
+            })
+        }
+    })
+
 });
 
-// Deletes a sticky note
+// Deletes a stickynote
 router.delete('/:id', function(req, res, next) {
 
-    res.json({
-        todo: true
-    });
+    // Delete stickynote
+    stickyNotes.removeStickyNote({
+        stickyNoteId: req.params('id')
+    }, function(err, response) {
+        if (err) {
+            return res.json({
+                success: false,
+                message: err
+            })
+        } else {
+            return res.json({
+                success: response.success
+            })
+        }
+    })
+
 });
 
 module.exports = router;
