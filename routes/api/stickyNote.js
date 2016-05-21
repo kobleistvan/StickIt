44075d481d70339840f2edd92feb8cae5885d96c
Koble Istvan <kobleistvan@gmail.com>
2016-05-21 07:24:00 -0700

    DB starting to work

--- routes/api/stickyNote.js
+++ routes/api/stickyNote.js
@@ -1,5 +1,6 @@
 var express = require('express'),
     router = express.Router(),
+    stickyNotes = rootRequire('lib/api/stickyNote'),
     logger = require('winston');
 
 // RESTful handlers to manage stickynotes
@@ -24,9 +25,31 @@ router.get('/:id', function(req, res, next) {
 // Creates a new sticky note
 router.post('/', function(req, res, next) {
 
-    res.json({
-        todo: true
-    });
+    if (!req.body.note) {
+        return res.json({
+            success: false,
+            message: "Sticky note is empty."
+        })
+    } else {
+        stickyNotes.addStickyNote({
+            note: req.body.note
+        }, function(err, response) {
+            if (err) {
+                logger.error("An error occured while creating a new sticky note. Err: ", err);
+
+                return res.json({
+                    success: false,
+                    message: err
+                })
+
+            } else {
+                return res.json({
+                    success: true
+                })
+            }
+        })
+    }
+
 });
 
 // Updates a sticky note
