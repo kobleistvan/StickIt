f681ee5cf2ea08186ff77e5bbe2a60c0e96abc64
Koble Istvan <kobleistvan@gmail.com>
2016-05-23 13:22:01 -0700

    Styled stickynotes and lastModified. Need to finish up post + put

--- public/javascripts/components/stickyNotes.js
+++ public/javascripts/components/stickyNotes.js
@@ -2,6 +2,27 @@ function StickyNotesController($scope, $http, $rootScope) {
 
     $scope.stickyNotes = [];
 
+    $scope.delete = function(stickyNote) {
+        $http.delete('/api/stickynote/'+stickyNote.id).then(function(result) {
+            if (result.data.success) {
+                var idx = $scope.stickyNotes.indexOf(stickyNote);
+                if (idx >= 0) {
+                  $scope.stickyNotes.splice(idx, 1);
+                }
+
+                return $rootScope.$broadcast("alert:add", {
+                    msg: "Sticky note successfully deleted.",
+                    type: "success"
+                });
+            } else {
+                return $rootScope.$broadcast("alert:add", {
+                    msg: result.err,
+                    type: "danger"
+                });
+            }
+        });
+    };
+
     this.fetchNotes = function() {
         $http.get('/api/stickynote/').then(function(result) {
             if (result.data.success) {
